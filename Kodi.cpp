87eb10315b401fbbff6ad02c3367168535343bcd
Fisnik369 <59198126+Fisnik369@users.noreply.github.com>
2020-03-29 19:08:16 +0200

    Update Kodi.cpp

--- Kodi.cpp
+++ Kodi.cpp
@@ -32,6 +32,54 @@ string convertfortap(char c, int ntap)
     }
     return tap;
 }
+//function to encrypt the plain text
+string encrypt(string x,int n)
+    {
+    string cipher="";
+
+    for(int i=0;i<x.length();i++)
+        {
+        if(isupper(x[i]))
+            cipher += (x[i] + n - 65)%26 + 65;
+        else if(islower(x[i]))
+            cipher += (x[i] + n - 97)%26 + 97;
+        else
+            cipher += x[i];
+        }
+    return cipher;
+    }
+
+//function to decrypt the cipher text using brute force attack
+void decrypt(string x)
+    {
+    string text;
+    for(int n=0;n<26;n++)
+        {
+        text = "";
+        for(int i=0;i<x.length();i++)
+            {
+            if(isupper(x[i]))
+                {
+                if((x[i] - n - 65)<0)
+                    text += 91 + (x[i] - n - 65);
+                else
+                    text += (x[i] - n - 65)%26 + 65;
+                }
+            else if(islower(x[i]))
+                {
+                if((x[i] - n - 97) < 0)
+                    text += 123 + (x[i] - n - 97);
+                else
+                    text += (x[i] - n - 97)%26 + 97;
+                }
+            else
+                text += x[i];
+            }
+        cout << "plain text for key " << n << " :- " << text << endl;
+        }
+    }
+
+
 
 int main(int argc, char*argv[]){
 
@@ -102,9 +150,28 @@ int main(int argc, char*argv[]){
         cout << endl;
         cout << "Mesazhi i dekriptuar: " << decryptedText << endl;
 
-    } else {
-        cout << "Zgjedhje e gabuar";
+    } else if(argv[2][0] == 'b'){
+        else if(argv[2][0] == 'b') {
+        int key = atoi(argv[3]);
+
+string text = argv[4];
+
+
+
+ encrypt(text,key);
+cout << "cipher text :- " << encrypt(text, key) << endl << endl;
+
+decrypt(encrypt(text, key));
+
     }
+
+        
+    }
+       else{
+       
+           cout << "Zgjedhje e gabuar!";
+       
+       }
 
     }
   
